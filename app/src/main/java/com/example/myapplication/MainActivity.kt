4718ac1626cdaf2cdc58bd5f661a6e0a87df4678
Aaron Myhrberg <aaronmyhrberg@gmail.com>
2025-06-05 17:23:07 +0200

    played around with colors

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -5,20 +5,23 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -52,7 +55,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun GreetingScreen(onNavigate: () -> Unit){
     Surface(
-        color = Color.Green,
+        color = Color(174, 200, 164),
         modifier = Modifier.fillMaxSize()
     ){
         Column(
@@ -60,9 +63,15 @@ fun GreetingScreen(onNavigate: () -> Unit){
             verticalArrangement = Arrangement.Center,
             modifier = Modifier.fillMaxSize()
         ){
-            Text("Hi, Welcome To Flowery!")
+            Text(
+                "Hi, Welcome To Flowery!",
+                color = Color(59, 59, 26),
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold
+            )
             Spacer(modifier = Modifier.height(16.dp))
-            Button(onClick = onNavigate) {
+            Button(onClick = onNavigate,
+                colors =  ButtonDefaults.buttonColors(Color(59, 59, 26))) {
                 Text("Go to Second Screen")
             }
         }
@@ -73,7 +82,7 @@ fun GreetingScreen(onNavigate: () -> Unit){
 @Composable
 fun SecondScreen(onBack: () -> Unit) {
     Surface(
-        color = Color.Cyan,
+        color = Color(231, 239, 199),
         modifier = Modifier.fillMaxSize()
     ) {
         Column(
@@ -81,9 +90,13 @@ fun SecondScreen(onBack: () -> Unit) {
             verticalArrangement = Arrangement.Center,
             modifier = Modifier.fillMaxSize()
         ) {
-            Text("Wanna add some flowers? Cant do that yet...!")
+            Text("Wanna add some flowers? Cant yet!",
+                color = Color(59, 59, 26),
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold)
             Spacer(modifier = Modifier.height(16.dp))
-            Button(onClick = onBack) {
+            Button(onClick = onBack,
+                colors =  ButtonDefaults.buttonColors(Color(59, 59, 26))) {
                 Text("Go back")
             }
         }
