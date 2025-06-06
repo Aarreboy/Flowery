37b81994793fe99839dbb68fdca7a3073662a967
Aaron Myhrberg <aaronmyhrberg@gmail.com>
2025-06-06 16:31:30 +0200

    added new pages, no function yet. Added icons for funsies

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,14 +1,18 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Surface
@@ -17,8 +21,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -28,22 +32,30 @@ import androidx.navigation.compose.rememberNavController
 import com.example.myapplication.ui.theme.MyApplicationTheme
 
 
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
         setContent {
             MyApplicationTheme {
                 val navController = rememberNavController()
                 NavHost(navController = navController, startDestination = "greeting") {
-                    composable("greeting"){
+                    composable("greeting") {
                         GreetingScreen(
-                            onNavigate = {navController.navigate("second")}
+                            onNavigate = { route -> navController.navigate(route) }
                         )
                     }
-                    composable("second"){
-                        SecondScreen(
-                            onBack = {navController.popBackStack()}
+                    composable("second") {
+                        ManagePlants(
+                            onBack = { navController.popBackStack() },
+                            onPlant = { Toast.makeText(this@MainActivity, "Planted a new flower!", Toast.LENGTH_SHORT).show() }
+                        )
+                    }
+                    composable("WaterPlants") {
+                        WaterPlants(
+                            onBack = { navController.popBackStack() }
                         )
                     }
                 }
@@ -52,17 +64,25 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+
 @Composable
-fun GreetingScreen(onNavigate: () -> Unit){
+fun GreetingScreen(onNavigate: (String) -> Unit){
     Surface(
         color = Color(174, 200, 164),
         modifier = Modifier.fillMaxSize()
     ){
         Column(
             horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center,
-            modifier = Modifier.fillMaxSize()
+            verticalArrangement = Arrangement.Top,
+            modifier = Modifier.fillMaxSize().padding(top = 64.dp)
         ){
+            Image(
+                painter = painterResource(id = R.drawable.flowerroseplant),
+                contentDescription = "App Icon",
+                modifier = Modifier.size(120.dp).padding(bottom = 16.dp),
+                contentScale = ContentScale.Fit
+            )
+
             Text(
                 "Hi, Welcome To Flowery!",
                 color = Color(59, 59, 26),
@@ -70,9 +90,13 @@ fun GreetingScreen(onNavigate: () -> Unit){
                 fontWeight = FontWeight.Bold
             )
             Spacer(modifier = Modifier.height(16.dp))
-            Button(onClick = onNavigate,
+            Button(onClick = {onNavigate("second") },
                 colors =  ButtonDefaults.buttonColors(Color(59, 59, 26))) {
-                Text("Go to Second Screen")
+                Text("Add/Remove Plants")
+            }
+            Button(onClick = {onNavigate("WaterPlants")},
+                colors =  ButtonDefaults.buttonColors(Color(59, 59, 26))) {
+                Text("Water Plants")
             }
         }
 
@@ -80,17 +104,60 @@ fun GreetingScreen(onNavigate: () -> Unit){
 }
 
 @Composable
-fun SecondScreen(onBack: () -> Unit) {
+fun ManagePlants(onBack: () -> Unit, onPlant: () -> Unit) {
     Surface(
         color = Color(231, 239, 199),
         modifier = Modifier.fillMaxSize()
     ) {
         Column(
             horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center,
-            modifier = Modifier.fillMaxSize()
+            verticalArrangement = Arrangement.Top,
+            modifier = Modifier.fillMaxSize().padding(top = 64.dp)
         ) {
+
+            Image(
+                painter = painterResource(id = R.drawable.gardeningtools),
+                contentDescription = "App Icon",
+                modifier = Modifier.size(120.dp).padding(bottom = 16.dp),
+                contentScale = ContentScale.Fit
+            )
+
             Text("Wanna add some flowers? Cant yet!",
+                color = Color(59, 59, 26),
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold)
+            Spacer(modifier = Modifier.height(16.dp))
+            Button(onClick = onPlant,
+                colors =  ButtonDefaults.buttonColors(Color(1, 59, 26))) {
+                Text("Plant")
+            }
+            Button(onClick = onBack,
+                colors =  ButtonDefaults.buttonColors(Color(59, 59, 26))) {
+                Text("Go back")
+            }
+        }
+    }
+}
+
+@Composable
+fun WaterPlants(onBack: () -> Unit) {
+    Surface(
+        color = Color(231, 239, 199),
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Top,
+            modifier = Modifier.fillMaxSize().padding(top = 64.dp)
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.watercan),
+                contentDescription = "App Icon",
+                modifier = Modifier.size(120.dp).padding(bottom = 16.dp),
+                contentScale = ContentScale.Fit
+            )
+
+            Text("Wanna Water some flowers? Cant yet!",
                 color = Color(59, 59, 26),
                 fontSize = 20.sp,
                 fontWeight = FontWeight.Bold)
